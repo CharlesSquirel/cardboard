84cedc3de9469a00f8c18fdbe6cc1c114024c5a6
unknown <karol.wiewiorka@gmail.com>
2023-11-24 17:46:14 +0100

    integration test add card

--- src/components/Board/__tests__/integration/Board.container.test.tsx
+++ src/components/Board/__tests__/integration/Board.container.test.tsx
@@ -1,3 +1,22 @@
+import { vi } from "vitest"
+import * as dataModule from '../../../../data'
+import { render, waitFor, screen, fireEvent } from "@testing-library/react"
+import BoardContainer from "../../Board.container"
+
 describe('BoardContainer integration tests', () => {
-  // TODO: Add unit tests
+  it("if clicking on <AddNewCard /> adds new card to database, and displays it correctly in <Board />", async() => {
+    vi.spyOn(dataModule, 'findCards').mockResolvedValue([])
+    const createCardSpy = vi
+      .spyOn(dataModule.CardModel.prototype, 'create')
+      .mockResolvedValue()
+    render(<BoardContainer/>)
+    await waitFor(() => {
+      const addBtn = screen.getByRole("button", {
+        name: "Add new card"
+      })
+      fireEvent.click(addBtn)
+      expect(screen.getByText("Click to start noting")).toBeInTheDocument()
+    })
+  })
+  
 })
