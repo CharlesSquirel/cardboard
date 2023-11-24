8f3f9a590ab399aa16c40203b2279f59a6750a23
Karol Wiewiórka <karol.wiewiorka@gmail.com>
2023-11-24 21:50:24 +0100

    refactoring code

--- src/components/Board/__tests__/integration/Board.container.test.tsx
+++ src/components/Board/__tests__/integration/Board.container.test.tsx
@@ -1,50 +1,52 @@
-import { vi } from "vitest"
+import { vi } from 'vitest'
 import * as dataModule from '../../../../data'
-import { render, waitFor, screen, fireEvent } from "@testing-library/react"
-import BoardContainer from "../../Board.container"
+import { render, waitFor, screen, fireEvent } from '@testing-library/react'
+import BoardContainer from '../../Board.container'
 
 describe('BoardContainer integration tests', () => {
-  it("if clicking on <AddNewCard /> adds new card to database, and displays it correctly in <Board />", async() => {
+  it('if clicking on <AddNewCard /> adds new card to database, and displays it correctly in <Board />', async () => {
     vi.spyOn(dataModule, 'findCards').mockResolvedValue([])
     const createCardSpy = vi
       .spyOn(dataModule.CardModel.prototype, 'create')
       .mockResolvedValue()
-    render(<BoardContainer/>)
+    render(<BoardContainer />)
     await waitFor(() => {
-      const addBtn = screen.getByRole("button", {
-        name: "Add new card"
+      const addBtn = screen.getByRole('button', {
+        name: 'Add new card',
       })
       fireEvent.click(addBtn)
-      expect(screen.getByText("Click to start noting")).toBeInTheDocument()
+      expect(screen.getByText('Click to start noting')).toBeInTheDocument()
     })
     expect(createCardSpy).toHaveBeenCalledTimes(1)
   })
-  it("update selected card content correctly", async () => {
+  it('update selected card content correctly', async () => {
     const mockedNewText = 'new text'
+
+    // Mock necessary functions
     vi.spyOn(dataModule, 'findCards').mockResolvedValue([])
     const createCardSpy = vi
-    .spyOn(dataModule.CardModel.prototype, 'create')
-    .mockResolvedValue()
+      .spyOn(dataModule.CardModel.prototype, 'create')
+      .mockResolvedValue()
     const updateCardSpy = vi
-    .spyOn(dataModule.CardModel.prototype, 'update')
-    .mockResolvedValue()
-    render(<BoardContainer/>)
-    const addBtn = screen.getByRole("button", {
-      name: "Add new card"
+      .spyOn(dataModule.CardModel.prototype, 'update')
+      .mockResolvedValue()
+    render(<BoardContainer />)
+    const addBtn = screen.getByRole('button', {
+      name: 'Add new card',
     })
+    fireEvent.click(addBtn)
     await waitFor(() => {
-      fireEvent.click(addBtn)
-      const card = screen.getByText("Click to start noting")
+      const card = screen.getByText('Click to start noting')
       fireEvent.click(card)
-      const textarea = screen.getByRole("textbox")
-      fireEvent.change(textarea, {
-        target:{
-          name: mockedNewText
-        }
-    })
-    fireEvent.blur(card)
-    const newCard = screen.getByText(mockedNewText)
-  })
-  })
 
+      const textarea = screen.getByRole('textbox')
+      fireEvent.change(textarea, {
+        target: {
+          value: mockedNewText,
+        },
+      })
+      const newCard = screen.getByText(mockedNewText)
+      expect(newCard).toBeInTheDocument()
+    })
+  })
 })
