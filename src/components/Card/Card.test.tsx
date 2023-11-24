a9f50466e496821fd5f21d33151c7ed56460a515
unknown <karol.wiewiorka@gmail.com>
2023-11-24 15:55:01 +0100

    unit test ending

--- src/components/Card/Card.test.tsx
+++ src/components/Card/Card.test.tsx
@@ -1,6 +1,7 @@
 import { fireEvent, render, screen } from '@testing-library/react'
 import { createCard } from '../../data/card/factory'
 import { Card } from './Card'
+import {vi} from "vitest"
 
 describe('Card component unit tests', () => {
   const mockedCard = createCard({
@@ -17,7 +18,24 @@ describe('Card component unit tests', () => {
     render(<Card {...mockedCard} />)
     const card = screen.getByText('test content')
     fireEvent.click(card)
-    fireEvent.blur(card)
+    fireEvent.blur(screen.getByRole("textbox"))
     expect(card).not.toBeInTheDocument()
+  })
+  it("if onUpdateCard is being fired when exiting edit mode", () => {
+    const mockUpdateCard = vi.fn()
+    render(<Card {...mockedCard} onUpdateCard={mockUpdateCard}/>)
+    const card = screen.getByText('test content')
+    fireEvent.click(card)
+    fireEvent.blur(screen.getByRole("textbox"))
+    expect(mockUpdateCard).toHaveBeenCalled()
+  })
+  it("if value on textarea is being set when firing change event in edit mode", () => {
+    const mockedNewText = "new content";
+    render(<Card {...mockedCard} />)
+    const card = screen.getByText('test content')
+    fireEvent.click(card)
+    const textarea = screen.getByRole("textbox")
+    fireEvent.change(textarea,{target:{value: mockedNewText}})
+    expect(textarea.textContent).toBe(mockedNewText)
   })
 })
